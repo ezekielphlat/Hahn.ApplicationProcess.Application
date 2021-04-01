ee29fc08dd4df715651a4d81e6bddc1a05c24e89
Ezekiel <ezekielphlat@gmail.com>
2021-04-01 22:40:40 +0100

    configure Inmemory entity core

---
+++ Hahn.ApplicationProcess.February2021.Data/Data/AssetDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Hahn.ApplicationProcess.February2021.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hahn.ApplicationProcess.February2021.Data.Data
+{
+    public class AssetDbContext : DbContext
+    {
+        public AssetDbContext(DbContextOptions<AssetDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Asset> Assets { get; set; }
+
+        public void LoadDummyAssets()
+        {
+            Asset asset = new Asset() { ID = 1, AssetName = "My AssetName", Department = "HQ" };
+            Assets.Add(asset);
+        }
+        public List<Asset> GetAssets()
+        {
+            return Assets.Local.ToList<Asset>();
+        }
+    }
+}
---
+++ Hahn.ApplicationProcess.February2021.Data/Models/Asset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Hahn.ApplicationProcess.February2021.Data.Models
+{
+    public class Asset
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string AssetName { get; set; }
+        public string Department { get; set; }
+        public string CountryOfDepartment { get; set; }
+        public string EmailAddressOfDepartment { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public bool Broker { get; set; } = false;
+
+    }
+}
