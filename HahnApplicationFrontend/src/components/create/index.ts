877e9d0b435bb4016a17d7cf00521a5a1149482f
Ezekiel <eabodesegun@appzonegroup.com>
2021-04-06 19:17:41 +0100

    add validation functionality

--- HahnApplicationFrontend/src/components/create/index.ts
+++ HahnApplicationFrontend/src/components/create/index.ts
@@ -1,10 +1,38 @@
-export class Create{
-    name = "";
-    department = "";
-    country = "";
-    email = "";
-    date = "";
-    isBroken ="";
-
+import {ValidationRules, ValidationControllerFactory} from 'aurelia-validation';
+import {inject} from 'aurelia-framework'
+import {bindable} from 'aurelia-framework'
+import { ObserverLocator } from 'aurelia-framework';
+@inject(ValidationControllerFactory,ObserverLocator)
+export class Create{ 
     
+    isBroken ="";
+    controller: any;
+    asset : any;
+    departments = [
+        { id: 1, name: 'HQ' },
+        { id: 2, name: 'Store1' },
+        { id: 3, name: 'Store2' },
+        { id: 4, name: 'Store3' },
+        { id: 5, name: 'MaintenanceStation' },
+      ];
+    selectedDepartmentID = null;
+     
+    constructor(validationControllerFactory,observerLocator){
+        this.controller = validationControllerFactory.createForCurrentScope();
+        var subscription = observerLocator
+            .getObserver(this, 'asset')
+            .subscribe(this.onAssetChange.bind(this));     
+
+              
+    }
+    
+    submitAsset(){
+
+    }
+    onAssetChange(){
+        if(this.asset){
+            ValidationRules.ensure('name')
+            .required().on(this.asset);
+        }
+    }
 }
