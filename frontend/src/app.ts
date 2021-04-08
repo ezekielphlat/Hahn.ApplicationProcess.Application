7b704999212d77e7df7fcd3d6bdf94f0e7890972
Ezekiel <ezekielphlat@gmail.com>
2021-04-08 13:17:54 +0100

    disable i18n

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,17 +1,13 @@
 import {PLATFORM} from 'aurelia-pal';
-import {I18N} from 'aurelia-i18n';
 require('bootstrap/dist/css/bootstrap.min.css');
 require('bootstrap');
 require('jquery');
 
-export class App {
-  static inject = [I18N];
+export class App {  
   
   router: any;
   i18n: any;
-  constructor(i18n){
-    this.i18n = i18n;    
-  }
+ 
   configureRouter(config, router){
     config.title = "Hahn Assets";
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,29 +8,29 @@ export function configure(aurelia: Aurelia): void {
     .standardConfiguration()
     .feature(PLATFORM.moduleName('resources/index'))
     .plugin(PLATFORM.moduleName('aurelia-dialog'))
-    .plugin(PLATFORM.moduleName('aurelia-validation'))
-    .plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
-      let aliases = ['t', 'i18n'];
-      // add aliases for 't' attribute
-      TCustomAttribute.configureAliases(aliases);
+    .plugin(PLATFORM.moduleName('aurelia-validation'));
+    // .plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
+    //   let aliases = ['t', 'i18n'];
+    //   // add aliases for 't' attribute
+    //   TCustomAttribute.configureAliases(aliases);
 
-      // register backend plugin
-      instance.i18next.use(Backend);
+    //   // register backend plugin
+    //   instance.i18next.use(Backend);
 
-      // adapt options to your needs (see http://i18next.com/docs/options/)
-      // make sure to return the promise of the setup method, in order to guarantee proper loading
-      return instance.setup({
-        // backend: {                                  // <-- configure backend settings
-        //   loadPath: './locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
-        // },
-        attributes: aliases,
-        lng : 'en-De',
-        fallbackLng : 'en',
-        ns:['nav', 'asset-form'],
-        defaultNS: 'translation',
-        debug:false
-      });
-    });
+    //   // adapt options to your needs (see http://i18next.com/docs/options/)
+    //   // make sure to return the promise of the setup method, in order to guarantee proper loading
+    //   return instance.setup({
+    //     backend: {                                  // <-- configure backend settings
+    //       loadPath: './locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+    //     },
+    //     attributes: aliases,
+    //     lng : 'en-De',
+    //     fallbackLng : 'en',
+    //     ns:['nav', 'asset-form'],
+    //     defaultNS: 'translation',
+    //     debug:false
+    //   });
+    // });
 
   aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
